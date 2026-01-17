c22d1b0acb3c7303121c425a5ce7c81c9f574370
Frankelly Cordero <frankellycordero88@gmail.com>
2026-01-16 22:08:41 -0400

    Set up testing

--- meadowlark.js
+++ meadowlark.js
@@ -1,66 +1,22 @@
 const express = require("express")
-const colors = require("colors")
 const expressHandlebars = require("express-handlebars")
+const handlers = require("./lib/handlers")
 
 const port = process.env.PORT || 2323
 const app = express()
-//
-// app.get("/", (req, res)=> {
-//     res.type("text/plain")
-//     res.send("Frankelly's Meadowlark travel & Co.")
-// })
-//
-// app.get("/about", (req, res) => {
-//   res.type("text/plain")
-//   res.send("Frankelly's Meadowlark about page")
-// })
-//
-// app.use((req, res) => {
-//   res.type("text/plain")
-//   res.status(404)
-//   res.send("404 - Not found")
-// })
-//
-//
-// app.use((err, req, res, next) => {
-//   res.type("text/plain")
-//   res.status(500)
-//   res.send("Internal server Error")
-// })
-//
-
-
-// using handle bars
-const fortune = ["Keep smiling, because life is a beautiful thing and there's so much to smile about. - Marilyn Monroe","Knowing your own darkness is the best method for dealing with the darknesses of other people. - Carl Jung", "The fear of death is more to be feared, than death itself. - Publilius Syrus","Our virtues and our failings are inseparable, like force and matter. When they separate, man is no more.  - Nikola Tesla","There is only one thing that makes a dream impossible to achieve: the fear of failure. - Paulo Coelho"]
-
-
 
 app.engine("handlebars", expressHandlebars.engine({
   defaultLayout: "main",
 }))
 
 app.set("view engine", "handlebars")
-
 app.use(express.static(__dirname + "/public"))
 
-app.get("/", (req, res) => res.render("home"))
-app.get("/about", (req, res) => {
-  const randomFortune = fortune[Math.floor(Math.random() * fortune.length)]
-  res.render("about", {fortune: randomFortune})
+app.get("/", handlers.home)
+app.get("/about", handlers.about)
 
-})
+app.use(handlers.notFound)
+app.use(handlers.internalError)
 
-app.use((req, res) => {
-  res.status(404)
-  res.render("404")
-})
-
-app.use((err, req, res, next) => {
-
-  console.error(err);
-  res.status(500)
-  res.render("500")
-})
-
-app.listen(port, ()=> console.log("Listening on localhost:".blue + port))
+app.listen(port, ()=> console.log("Listening on localhost:" + port))
 
